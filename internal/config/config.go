56542cc919a65bcd261354b57af58ec160891869
Liam Bryant <liam.bryant@dgsspa.com>
2025-12-22 13:30:33 +0100

    feat: add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,39 @@
 package config
 
 import (
+	"fmt"
+	"log/slog"
+
+	"github.com/joho/godotenv"
 	"go-simpler.org/env"
 )
 
 type LogLevel string
 
-var cfg struct {
+var Cfg struct {
 	LogLevel LogLevel `env:"LOG_LEVEL" default:"INFO"`
+	DBDSN    string   `env:"DB_DSN" default:"file:cardman.db?_fk=1"`
 }
 
 func LoadConfig() {
-	if err := env.Load(&cfg, &env.Options{}); err != nil {
+	_ = godotenv.Load()
+	if err := env.Load(&Cfg, &env.Options{}); err != nil {
 		panic(err)
+	}
+	fmt.Printf("Current configuration: %+v\n", Cfg)
+}
+
+func GetLogLevel() slog.Level {
+	switch Cfg.LogLevel {
+	case "DEBUG":
+		return slog.LevelDebug
+	case "INFO":
+		return slog.LevelInfo
+	case "WARN":
+		return slog.LevelWarn
+	case "ERROR":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
 	}
 }
